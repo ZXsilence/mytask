0745fb77ee4beb2131972ec077a0cdf24b86ae84
liyangmin <liyangmin@maimiaotech.com>
2013-01-09 19:39:36 +0800

    支持simba_insight_wordsbase_get 分页

--- tao_models/simba_insight_wordsbase_get.py
+++ tao_models/simba_insight_wordsbase_get.py
@@ -19,24 +19,38 @@ from tao_models.conf.settings import taobao_client
 from tao_models.common.decorator import  tao_api_exception
 
 class SimbaInsightWordsbaseGet(object):
+    
+    MAX_WORDS = 170
 
     @classmethod
     @tao_api_exception(5)
-    def get_words_base(cls, access_token, time, word_list):
-        """
-        get words base 
-        """
+    def _get_words_base(cls, access_token, time, word_list):
+        '''get words base'''
         req = SimbaInsightWordsbaseGetRequest()
         req.time = time
         req.words = ','.join(word_list)
         req.filter = 'PV|CLICK|AVGCPC|COMPETITION'
-
         rsp = taobao_client.execute(req, access_token)[0]
         if not rsp.isSuccess():
             raise ErrorResponseException(code=rsp.code, msg=rsp.msg, sub_code=rsp.sub_msg, sub_msg=rsp.sub_msg)
 
-        print 'success .....'
         return rsp.in_word_bases
+
+    @classmethod
+    def get_words_base(cls, access_token, time, word_list):
+        """
+        get words base 
+        """
+        word_list = copy.deepcopy(word_list)
+        total_list = []
+       
+        while word_list:
+            sub_word_list = word_list[:cls.MAX_WORDS]
+            word_list = word_list[cls.MAX_WORDS:]
+            sub_list = cls._get_words_base(access_token, time, sub_word_list)
+            total_list.extend(sub_list)
+
+        return total_list
 
 if __name__ == '__main__':
     access_token = "620260146ZZc0465e1b4185f7b4ca8ba1c7736c28d1c675871727117"
