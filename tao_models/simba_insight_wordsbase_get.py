702b448f79dd6d350d9e5369287a59695b686eeb
liyangmin <liyangmin@maimiaotech.com>
2013-01-09 17:24:16 +0800

    modified:   tao_models/simba_insight_wordsbase_get.py    不需要传入accesstoken nick 信息    word list 传入python list

--- tao_models/simba_insight_wordsbase_get.py
+++ tao_models/simba_insight_wordsbase_get.py
@@ -22,14 +22,14 @@ class SimbaInsightWordsbaseGet(object):
 
     @classmethod
     @tao_api_exception(5)
-    def get_words_base(cls, access_token, nick, time, words):
+    def get_words_base(cls, time, word_list):
         """
         get words base 
         """
         req = SimbaInsightWordsbaseGetRequest()
         req.nick = nick
         req.time = time
-        req.words = words
+        req.words = ','.join(word_list)
         req.filter = 'PV|CLICK|AVGCPC|COMPETITION'
 
         rsp = taobao_client.execute(req, access_token)[0]
@@ -42,10 +42,13 @@ class SimbaInsightWordsbaseGet(object):
 if __name__ == '__main__':
     access_token = "620260146ZZc0465e1b4185f7b4ca8ba1c7736c28d1c675871727117"
     nick = '牙齿天天晒'
-    word_info_list = SimbaInsightWordsbaseGet.get_words_base(access_token, nick, '3MONTH', '登山鞋,冲锋衣,登山包,户外鞋,徒步鞋')
+    word_info_list = SimbaInsightWordsbaseGet.get_words_base( 'WEEK', ['nifeifie登山鞋','冲锋衣','登山包','户外鞋','徒步鞋'])
     print 'word_info_list', word_info_list
     for word_info in word_info_list:
+        print word_info.toDict()
         print  word_info.word
         for x in word_info.in_record_base_list:
+            print x.toDict()
+            print type(x.date)
             print x.date, x.pv
 
