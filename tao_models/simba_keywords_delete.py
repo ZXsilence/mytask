0fe3ccced79930aa4c37e1ae4d5bc6b2981404c7
chenke <chenke@maimiaotech.com>
2013-01-15 14:03:31 +0800

    simba keywords delete 支持分页

--- tao_models/simba_keywords_delete.py
+++ tao_models/simba_keywords_delete.py
@@ -25,22 +25,49 @@ from tao_models.common.decorator import  tao_api_exception
 logger = logging.getLogger(__name__)
 
 class SimbaKeywordsDelete(object):
+    """
+    TODO
+    """
 
     @classmethod
     @tao_api_exception()
-    def delete_keywords(cls, access_token, nick, campaign_id, keyword_id_list):
-        if not keyword_id_list:
-            return []
-        keyword_id_list = [str(keyword_id) for keyword_id in keyword_id_list]
-        req = SimbaKeywordsDeleteRequest()
-        req.campaign_id = campaign_id
-        req.nick = nick
-        req.keyword_ids = ','.join(keyword_id_list)
+    def _delete_keywords(cls, access_token, nick, campaign_id, keyword_id_list):
+        """
+        args:
+            keyword_id_list: [200000001,200000002]
+        """
 
-        rsp = taobao_client.execute(req, access_token)[0]
+        word_list = [str(word) for word in keyword_id_list]
+
+        req = SimbaKeywordsDeleteRequest()
+        req.nick = nick
+        req.campaign_id = campaign_id
+        req.keyword_ids = ",".join(word_list)
+
+        try:
+            rsp = taobao_client.execute(req, access_token)[0]
+        except Exception, data:
+            raise ApiExecuteException
+
         if not rsp.isSuccess():
-            if rsp.code == 15 and rsp.sub_msg == u'没有属于该客户下指定推广计划的有效关键词可删除':
+            if rsp.code == 15 and rsp.sub_msg == u'没有有效关键词可增加， 输入的关键词和已有出现重复':
                 return []
-            raise ErrorResponseException(code=rsp.code, msg=rsp.msg, sub_code=rsp.sub_msg, sub_msg=rsp.sub_msg)
+            logger.error("delete_keywords error nick [%s] msg [%s] sub_msg [%s]" %(nick
+                , rsp.msg, rsp.sub_msg))
+            raise ErrorResponseException(code=rsp.code,msg=rsp.msg, sub_msg=rsp.sub_msg, sub_code=rsp.sub_code)
         return rsp.keywords
+
+    @classmethod
+    def delete_keywords(cls, access_token, nick, campaign_id, keyword_id_list):
+        keywords = []
+        package_num = len(keyword_id_list)/100 + 1
+        if len(keyword_id_list) % 100 == 0:
+            package_num -= 1
+        for i in range(package_num):
+            subkeywords = SimbaKeywordsDelete._delete_keywords(access_token, nick, \
+                    campaign_id, keyword_id_list[i*100: (i+1)*100])
+            keywords.extend(subkeywords)
+
+        return keywords
+
 
