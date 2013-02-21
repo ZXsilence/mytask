3c3a6bce9907d9b8391eaffece486f7d7a6656ee
wulingping <wulingping@maimiaotech.com>
2013-02-21 13:17:35 +0800

    添加删除创意接口

--- tao_models/simba_creative_add.py
+++ tao_models/simba_creative_add.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     from tao_models.conf.settings import set_taobao_client
     set_taobao_client('12651461', '80a15051c411f9ca52d664ebde46a9da')
 
-from TaobaoSdk import SimbaCreativesGetRequest
+from TaobaoSdk import SimbaCreativeAddRequest
 from TaobaoSdk.Exceptions import  ErrorResponseException
 
 from tao_models.conf.settings import taobao_client
@@ -21,41 +21,35 @@ from tao_models.common.decorator import  tao_api_exception
 
 logger = logging.getLogger(__name__)
 
-class SimbaCreativesGet(object):
+class SimbaCreativeAdd(object):
 
-    PAGE_SIZE = 200
 
     @classmethod
     @tao_api_exception(5)
-    def get_creative_list_by_adgroup(cls, access_token, nick, adgroup_id):
-        """
-        given a adgroup_id, get the creative list in this adgroup
-        """
+    def add_creative(cls, access_token, nick, adgroup_id,title,img_url):
 
-        creative_list = []
-
-        req = SimbaCreativesGetRequest()
+        req = SimbaCreativeAddRequest()
         req.nick = nick
         req.adgroup_id = adgroup_id
-        #req.creative_ids = ",".join([str(k) for k in creative_ids])
+        req.title = title
+        req.img_url = img_url
 
         rsp = taobao_client.execute(req, access_token)[0]
 
         if not rsp.isSuccess():
-            logger.error("get_creative_list_by_adgroup error nick [%s] msg [%s] sub_msg [%s]" %(nick
+            logger.error("add creative error nick [%s] msg [%s] sub_msg [%s]" %(nick
                 , rsp.msg, rsp.sub_msg))
             raise ErrorResponseException(code=rsp.code, msg=rsp.msg, sub_code=rsp.sub_msg, sub_msg=rsp.sub_msg)
 
-        return rsp.creatives
+        return rsp.creative
 
 
 if __name__ == '__main__':
     nick = 'chinchinstyle'
     access_token = '6200e168f708b8167250268dfhe2555e99ed247caa1cdeb520500325'
     adgroup_id =164302433 
-    nick = 'chinchinstyle'
-    
-    creatives = SimbaCreativesGet.get_creative_list_by_adgroup(access_token, nick, adgroup_id)
+    title = '测试推广内容test'
+    img_url = 'http://img.taobaocdn.com/bao/uploaded/i1/520500325/T2ZBhTXXNbXXXXXXXX_!!520500325.jpg'
+    print (SimbaCreativeAdd.add_creative(access_token,nick,adgroup_id,title,img_url)).toDict() 
 
-    for creative in creatives:
-        print creative.toDict()
+
