9d15189e47c89457f0cebab829ed8d54116e9921
wulingping <wulingping@maimiaotech.com>
2013-11-05 17:33:59 +0800

    修复定向报表接口的bug

--- tao_models/simba_rpt_adgroupnonsearchbase_get.py
+++ tao_models/simba_rpt_adgroupnonsearchbase_get.py
@@ -53,7 +53,10 @@ class SimbaRptAdgroupnonsearchBaseGet(object):
             rsp = tao_model_settings.taobao_client.execute(req, access_token)[0]
             if not rsp.isSuccess():
                 raise ErrorResponseException(code=rsp.code, msg=rsp.msg, sub_code=rsp.sub_code, sub_msg=rsp.sub_msg)
-            l = json.loads(rsp.rpt_nonsearch_base_list.lower())
+            if not rsp.rpt_adgroup_nonsearch_base:
+                l = {}
+            else:
+                l = json.loads(rsp.rpt_adgroup_nonsearch_base.lower())
             if l == {}:
                 l = []
 
--- tao_models/simba_rpt_adgroupnonsearcheffect_get.py
+++ tao_models/simba_rpt_adgroupnonsearcheffect_get.py
@@ -53,7 +53,10 @@ class SimbaRptAdgroupnonsearchEffectGet(object):
             rsp = tao_model_settings.taobao_client.execute(req, access_token)[0]
             if not rsp.isSuccess():
                 raise ErrorResponseException(code=rsp.code, msg=rsp.msg, sub_code=rsp.sub_code, sub_msg=rsp.sub_msg)
-            l = json.loads(rsp.rpt_adgroup_nonsearch_effect.lower())
+            if not rsp.rpt_adgroup_nonsearch_effect:
+                l = {}
+            else:
+                l = json.loads(rsp.rpt_adgroup_nonsearch_effect.lower())
             if l == {}:
                 l = []
 
