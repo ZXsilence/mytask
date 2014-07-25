7ce51ac0753c0922dd95a12438716061d80d0c1b
xieguanfu <xieguanfu@maimiaotech.com>
2014-07-25 11:06:20 +0800

    推广组定向获取异常处理

--- tao_models/common/decorator.py
+++ tao_models/common/decorator.py
@@ -109,6 +109,9 @@ def tao_api_exception(MAX_RETRY_TIMES = 20):
                         elif api_method == 'taobao.simba.nonsearch.adgroupplaces.delete' and e.sub_msg \
                                 and u'当前推广计划不支持该操作' in e.sub_msg:
                             raise NonsearchNotOpenException
+                        elif api_method == 'taobao.simba.nonsearch.adgroupplaces.get' and e.sub_msg \
+                                and u'当前推广计划不支持该操作' in e.sub_msg:
+                            raise NonsearchNotOpenException
 
                     #异常状态的重试处理,不扔出自定义的业务异常
                     if (code == 530 or code == 46) and e.sub_code.startswith('isp'): 
