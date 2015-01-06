2df54e44477a8306ea795a6c69d971c7f6f48478
wulingping <wulingping@maimiaotech.com>
2015-01-06 15:06:38 +0800

    rpt_all数据库迁移至rds4实例

--- db_pool/op_mysql_proxy.sh
+++ db_pool/op_mysql_proxy.sh
@@ -31,6 +31,7 @@ function start_proxy(){
     mysql-proxy --proxy-address=localhost:4040 --proxy-backend-addresses=jconn2thvf8ua.mysql.rds.aliyuncs.com:3306 --daemon --admin-address=127.0.0.1:5050 --admin-username=maimiao_ops --admin-password=maimiaoadmin2014 --admin-lua-script=/usr/share/mysql-proxy/ro-pooling.lua --log-file=/alidata1/logs/mysql_proxy_rds1.log --log-level=info
     mysql-proxy --proxy-address=localhost:4041 --proxy-backend-addresses=jconncd2y38wt.mysql.rds.aliyuncs.com:3306 --daemon --admin-address=127.0.0.1:5051 --admin-username=maimiao_ops --admin-password=maimiaoadmin2014 --admin-lua-script=/usr/share/mysql-proxy/ro-pooling.lua --log-file=/alidata1/logs/mysql_proxy_rds2.log --log-level=info
     mysql-proxy --proxy-address=localhost:4042 --proxy-backend-addresses=jconnuq7wnycm.mysql.rds.aliyuncs.com:3306 --daemon --admin-address=127.0.0.1:5052 --admin-username=maimiao_ops --admin-password=maimiaoadmin2014 --admin-lua-script=/usr/share/mysql-proxy/ro-pooling.lua --log-file=/alidata1/logs/mysql_proxy_rds3.log --log-level=info
+    mysql-proxy --proxy-address=localhost:4043 --proxy-backend-addresses=jconn75wjq2eq.mysql.rds.aliyuncs.com:3306 --daemon --admin-address=127.0.0.1:5053 --admin-username=maimiao_ops --admin-password=maimiaoadmin2014 --admin-lua-script=/usr/share/mysql-proxy/ro-pooling.lua --log-file=/alidata1/logs/mysql_proxy_rds4.log --log-level=info
     sleep 5 
     pids=`ps -ef|grep $USER|grep "mysql-proxy"|grep -v grep|awk '{print $2}'`
     if [ "$pids" == '' ] ; then
