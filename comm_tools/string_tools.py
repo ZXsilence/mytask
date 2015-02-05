50645ecea8464ea8bbf95ed2c04912c429c32b22
zhoujiebing <zhoujiebing@maimiaotech.com>
2015-02-05 14:28:17 +0800

    部分乱码解决

--- comm_tools/string_tools.py
+++ comm_tools/string_tools.py
@@ -23,9 +23,15 @@ class StringTools(object):
         stop_data = file(stop_file).read().split('\n')
         word_data = file(word_file).read().split('\n')
         word_set = [w.decode('utf-8') for w in stop_data]
+        
         for line in word_data:
             line_data = line.split('\t')
             word = line_data[0]
+            if type(word) == str:
+                try:
+                    word = word.decode('utf-8')
+                except Exception:
+                    pass
             word_set.append(word)
 
         return set(word_set)
@@ -43,20 +49,27 @@ class StringTools(object):
             except Exception,e:
                 keyword2 = keyword_str.decode('gbk')
             
+            if keyword1 == keyword2:
+                return keyword1
+
             score1 = 0
             for word in keyword1:
-                if not word or word.isalnum():
-                    continue
-                if  word in word_set:
+                if not word or word.isdigit():
+                    score1 += 1
+                elif  word in word_set:
                     score1 += 1
             score2 = 0
             for word in keyword2:
-                if not word or word.isalnum():
-                    continue
-                if  word in word_set:
+                if not word or word.isdigit():
                     score2 += 1
-
-            keyword = keyword1 if score2 > score1 else keyword2
+                elif  word in word_set:
+                    score2 += 1
+            
+            score1 = float(score1) / len(keyword1)
+            score2 = float(score2) / len(keyword2)
+            keyword = keyword2 if score2 > score1 else keyword1
+            #if score1 == 0 and score2 == 0:
+            #    keyword = keyword2
 
         except Exception,e:
             keyword = ''
