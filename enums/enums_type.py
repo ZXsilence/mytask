12521b7895c7703b805a8c0afcdad80489a44ea2
xiegf <xieguanfu@163.com>
2015-03-25 15:10:42 +0800

    加大减少投入cpc_max调整参数

--- enums/enums_type.py
+++ enums/enums_type.py
@@ -157,6 +157,8 @@ class OperationType(object):
     STOP_ADGROUP = 512
     STOP_OPTIMIZE_ADGROUP = 513
     START_OPTIMIZE_ADGROUP_ONLY_PRICE = 514
+    RISE_ADGROUP_CPC_MAX = 515
+    DROP_ADGROUP_CPC_MAX = 515
 
 
     ADD_CAMPAIGN_NORMAL = 601
@@ -231,6 +233,8 @@ class OperationType(object):
         STOP_ADGROUP,
         STOP_OPTIMIZE_ADGROUP,
         START_OPTIMIZE_ADGROUP_ONLY_PRICE,
+        RISE_ADGROUP_CPC_MAX,
+        DROP_ADGROUP_CPC_MAX,
 
         START_CAMPAIGN,
         START_OPTIMIZE_CAMPAIGN,
@@ -489,6 +493,8 @@ OPTTYPE_COMMENT = {
     ,OperationType.OPEN_CAMPAIGN_PLATFORM_OPTIMIZE:"开启平台优化"
     ,OperationType.CLOSE_CAMPAIGN_PLATFORM_OPTIMIZE:"关闭平台优化"
     ,OperationType.ADD_NEW_CAMPAIGN_SETTINGS:"新设置自动计划"
+    ,OperationType.RISE_ADGROUP_CPC_MAX:"为了效果加大投入,推广单元最高出价增加10%"
+    ,OperationType.DROP_ADGROUP_CPC_MAX:"为了效果减少投入,推广单元最高出价减少10%"
 
 }
 
