d459f2a0808efb1dac7b6c1b858a13c0ddb486ab
liumc <liumingchao@maimiaotech.com>
2016-06-23 11:46:52 +0800

    小修改

--- tao_models/simba_keywords_realtime_ranking_batch_get.py
+++ tao_models/simba_keywords_realtime_ranking_batch_get.py
@@ -31,8 +31,16 @@ import simplejson as json
 class SimbaKeywordsRealtimeRankingBatchGet(object):
     
     @classmethod
-    @tao_api_exception(5)
     def get_keywords_rank(cls,nick,adgroup_id,keyword_ids):
+        res = []
+        while keyword_ids:
+            res.extend(cls._get_keywords_rank_sub(nick,adgroup_id,keyword_ids[:20]))
+            keyword_ids=keyword_ids[20:]
+        return res
+
+    @classmethod
+    @tao_api_exception(5)
+    def _get_keywords_rank_sub(cls,nick,adgroup_id,keyword_ids):
         req = SimbaKeywordsRealtimeRankingBatchGetRequest()
         req.nick = nick
         req.ad_group_id = adgroup_id
@@ -46,10 +54,12 @@ class SimbaKeywordsRealtimeRankingBatchGet(object):
 
 if __name__ == '__main__':
     nick = "麦苗科技001"
-    adgroup_id = 677615768 
+    adgroup_id = 693290415 
     #keyword_ids = [255805868242,255805868243,255805868246]
     #keyword_ids = [274468785611,274468785581,274468785614,274468785545]
-    keyword_ids = [677615768]
+    keyword_ids = [276642644500,276642644499,276642644498,276642644496,276642644495,276642644493,276642644492,276642644491,276642644490,276642644489,\
+                   276642644488,276642644487,276642644486,276642644485,276642644484,276642644483,276642644482,276642644479,276642644478,276642644477,276642644475,\
+                  276642644474]
     res = SimbaKeywordsRealtimeRankingBatchGet.get_keywords_rank(nick,adgroup_id,keyword_ids)
-    print res
+    print len(res)
 
