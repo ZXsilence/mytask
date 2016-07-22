944e31e86b6feb4677bce8400fed509e241296ff
xiegf <xieguanfu@163.com>
2016-07-22 15:00:44 +0800

    添加消息api

---
+++ tao_models/messages_consume.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: xieguanfu
+@contact: xieguanfu@maimiaotech.com
+@date: 2016-07-22 13:30
+@version: 0.0.0
+@license: Copyright Maimiaotech.com
+@copyright: Copyright Maimiaotech.com
+
+"""
+
+import os,sys
+import logging
+
+if __name__ == '__main__':
+    sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+    from api_server.conf import set_env
+    set_env.getEnvReady()
+    from api_server.conf.settings import set_api_source
+    set_api_source('normal_test')
+
+from TaobaoSdk import TmcMessagesConsumeRequest
+from TaobaoSdk.Exceptions import ErrorResponseException
+from tao_models.common.decorator import  tao_api_exception
+from api_server.services.api_service import ApiService
+from api_server.common.util import change_obj_to_dict_deeply
+from tao_models.num_tools import change2num
+
+logger = logging.getLogger(__name__)
+
+class MessagesConsume(object):
+
+    @classmethod
+    @tao_api_exception()
+    def consume_message(cls,group_name = None,page_size = 200):
+        req = TmcMessagesConsumeRequest()
+        req.quantity = int(page_size)
+        if group_name:
+            req.group_name = group_name
+        nick = None
+        soft_code = 'SYB'
+        rsp = ApiService.execute(req,nick,soft_code)
+        return change_obj_to_dict_deeply(rsp.messages)
+
+if __name__ == '__main__':
+    print MessagesConsume.consume_message(page_size = 10)
