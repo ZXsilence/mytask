f5b19d23c80a6b04820a353abe930056b0af5acf
liumc <liumingchao@maimiaotech.com>
2016-08-09 16:26:41 +0800

    人群报表接口

--- tao_models/simba_searchcrowd_batch_add.py
+++ tao_models/simba_searchcrowd_batch_add.py
@@ -8,6 +8,7 @@ import sys
 import os
 import logging
 import logging.config
+import simplejson as json
 if __name__ == '__main__':
     sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
     from api_server.conf import set_env
@@ -30,15 +31,19 @@ class SearchcrowdBatchAdd(object):
         req = SimbaSearchcrowdBatchAddRequest()
         req.nick = nick
         req.adgroup_id = adgroup_id
-        req.adgroup_targeting_tags = adgroup_targeting_tags
+        req.adgroup_targeting_tags = json.dumps(adgroup_targeting_tags)
         rsp = ApiService.execute(req,nick,soft_code)
-        import pdb; pdb.set_trace()  # XXX BREAKPOINT
         return change_obj_to_dict_deeply(rsp.adgrouptargetingtags)
 
 
 if __name__ == '__main__':
     nick = "麦苗科技001"
-    adgroup_id = 700306707
-    adgroup_targeting_tags = {"crowdDTO":{"templateId":"34","name":"领用618购物券的访客","tagList":[{"dimId":"114812","tagId":"1","tagName":"领用618购物券的访客","optionGroupId":"12827"}]},"isDefaultPrice":0,"discount":101}
+    adgroup_id = 700403273 
+    #adgroup_targeting_tags = [{"crowdDTO":{"templateId":"28","name":"浏览未购买店内商品的访客","tagList":[{"dimId":"100012","tagId":"",\
+    #"tagName":"浏览未购买店内商品的访客","optionGroupId":"1012"}]},"isDefaultPrice":0,"discount":101},{"crowdDTO":{"templateId":"34",\
+    #"name":"领用618购物券的访客","tagList":[{"dimId":"100150","tagId":"1","tagName":"领用618购物券的访客","optionGroupId":"2000"}]},\
+    #"isDefaultPrice":0,"discount":101}]     
+    adgroup_targeting_tags = [{"crowdDTO":{"templateId":"28","name":"浏览未购买店内商品的访客","tagList":[{"dimId":"100012","tagId":"",\
+    "tagName":"浏览未购买店内商品的访客","optionGroupId":"1012"},{"dimId":"100150","tagId":"1","tagName":"领用618购物券的访客","optionGroupId":"2000"}]},"isDefaultPrice":0,"discount":101}]
     soft_code = 'SYB'
     print SearchcrowdBatchAdd.searchcrowdbatch_add(nick, adgroup_id, adgroup_targeting_tags, soft_code)
