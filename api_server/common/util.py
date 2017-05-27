733403f45ef83efbdeb4f3ae24df236135723804
yeyuqiu <yeyuqiu@outlook.com>
2017-05-27 09:34:45 +0800

    function change_obj_to_dict_deeply code optimize

--- api_server/common/util.py
+++ api_server/common/util.py
@@ -9,24 +9,22 @@ import logging
 logger = logging.getLogger(__name__)
 
 def change_obj_to_dict_deeply(obj):
-    result_dict = {}
-    if type(obj) == type([]):
+    if isinstance(obj, list):
         #列表转换
         return [change_obj_to_dict_deeply(sub_obj) for sub_obj in obj]
     elif isinstance(obj,object) and obj.__class__.__module__.startswith('Domain'):
         result_dict = obj.toDict()
-    elif type(obj) == type({}):
+    elif isinstance(obj, dict):
         result_dict = obj
     else:
         #基本类型，无需转换
         return obj
-    keys = result_dict.keys()
-    for key in keys:
-        #迭代转换
-        if isinstance(result_dict[key],object) and result_dict[key].__class__.__module__.startswith('Domain'):
-            result_dict[key] = change_obj_to_dict_deeply(result_dict[key])
-        elif type(result_dict[key]) == type([]):
-            result_dict[key] = [change_obj_to_dict_deeply(obj) for obj in result_dict[key]]
+    for key, value in result_dict.iteritems():
+        # 迭代转换
+        if isinstance(value, object) and value.__class__.__module__.startswith('Domain'):
+            result_dict[key] = change_obj_to_dict_deeply(value)
+        elif isinstance(value, list):
+            result_dict[key] = [change_obj_to_dict_deeply(obj) for obj in value]
     return result_dict
 
 
