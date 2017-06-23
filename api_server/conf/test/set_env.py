68b4931dd190136806b68ef56f91d35cca2e48ee
tanglingling <tanglingling@maimiaotech.com>
2017-06-23 13:34:12 +0800

    新建test机器软链

---
+++ api_server/conf/test/set_env.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Li Yangmin
+@contact: liyangmin@maimiaotech.com
+@date: 2012-11-16 13:47
+@version: 0.0.0
+@license: Copyright maimiaotech.com
+@copyright: Copyright maimiaotech.com
+
+"""
+import os
+import sys
+
+currDir = os.path.normpath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.normpath(os.path.join(currDir,os.path.pardir))
+PROJECT_PAR = os.path.normpath(os.path.join(PROJECT_ROOT, os.path.pardir))
+PYTHON_SDK = os.path.normpath(os.path.join(currDir, '../../../TaobaoOpenPythonSDK/'))
+BACKENDS = os.path.normpath(os.path.join(currDir, '../../../backends/'))
+API_GEN = os.path.normpath(os.path.join(currDir, '../../../comm_lib/api_server/thrift/gen-py/'))
+    
+def getEnvReady():
+    sys.path.insert(0,PYTHON_SDK)
+    sys.path.insert(0,PROJECT_PAR)
+    sys.path.insert(0,BACKENDS)
+    sys.path.insert(0,API_GEN)
+
+
+def getJdEnvReady():
+    sys.path.insert(0,PYTHON_SDK)
+    sys.path.insert(0,PROJECT_PAR)
+    sys.path.insert(0,API_GEN)
