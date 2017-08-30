7286032a149767fa5002f812e388a01c4d7094a4
xiegf <xieguanfu@163.com>
2017-08-30 14:24:59 +0800

    直通车排名接口格式变化修改

--- tao_models/simba_keywords_realtime_ranking_batch_get.py
+++ tao_models/simba_keywords_realtime_ranking_batch_get.py
@@ -48,18 +48,25 @@ class SimbaKeywordsRealtimeRankingBatchGet(object):
         soft_code = None
         rsp = ApiService.execute(req,nick,soft_code)
         res = change_obj_to_dict_deeply(rsp.result)
-        return res.get('realtime_rank_list',[])
+        if type(res.get('realtime_rank_list',[])) == dict:
+            return res.get('realtime_rank_list').get('result')
+        else:
+            return res.get('realtime_rank_list',[])
 
 
 
 if __name__ == '__main__':
     nick = "麦苗科技001"
     adgroup_id = 693290415 
+    nick = '北京学海轩图书专营店'
+    adgroup_id = 724524280
     #keyword_ids = [255805868242,255805868243,255805868246]
     #keyword_ids = [274468785611,274468785581,274468785614,274468785545]
     keyword_ids = [276642644500,276642644499,276642644498,276642644496,276642644495,276642644493,276642644492,276642644491,276642644490,276642644489,\
                    276642644488,276642644487,276642644486,276642644485,276642644484,276642644483,276642644482,276642644479,276642644478,276642644477,276642644475,\
                   276642644474]
+    keyword_ids = [357362755666]
     res = SimbaKeywordsRealtimeRankingBatchGet.get_keywords_rank(nick,adgroup_id,keyword_ids)
     print len(res)
+    print res
 
